22b61a375c47bc6d134d7c4ee8033b1d7d3910dc
Kroxx974 <62585376+Kroxx974@users.noreply.github.com>
2025-11-06 22:51:44 +0400

    Update amelie.js

--- script/amelie.js
+++ script/amelie.js
@@ -12,8 +12,8 @@ class Root {
         this.y           = y;
         this.speedX      = Math.random()*2 - 1;
         this.speedY      = Math.random()*2 - 1;
-        this.maxSize     = Math.random()*2 + 1;
-        this.size        = Math.random() + 1;
+        this.maxSize     = Math.random()*2.2;
+        this.size        = Math.random() + 0.2;
         this.sizeSpeed   = Math.random()*0.1 + 0.05;
         this.angleX      = Math.random()*6.2;
         this.angleXSpeed = Math.random()*0.6 - 0.3;
@@ -52,7 +52,7 @@ class Flower {
         this.y = y;
         this.size = size*3;
         this.sizeSpeed = Math.random()*0.2+0.2;
-        this.maxFlowerSize = this.size + Math.random()*20;
+        this.maxFlowerSize = this.size + Math.random()*25;
         this.image = flowerImg;
         this.willFlower = null;
 
@@ -67,18 +67,18 @@ class Flower {
 
     }
 }
-
+let scale = 3*Math.min(canvas.width/1016,canvas.height/980)
 function X(t){
     return canvas.width/2+(-115*cos(1*t)+2*cos(2*t)-12*cos(3*t)+7*cos(4*t)-6*cos(5*t)+6*cos(6*t)-3*cos(7*t)+3*cos(8*t)
     -cos(9*t)-2*cos(10*t)+4*cos(11*t)+cos(12*t)-2*cos(13*t)+8*cos(14*t)+2*cos(15*t)-cos(16*t)+5*cos(17*t)+2*cos(18*t)
     -cos(19*t)+3*cos(20*t)-cos(21*t)+cos(22*t)-2*cos(23*t)+4*cos(24*t)-2*cos(25*t)+2*cos(26*t)-3*cos(27*t)
-    +2*cos(28*t)-cos(29*t)+cos(30*t)-cos(31*t)+cos(32*t)-cos(33*t)+cos(34*t)-cos(35*t)+cos(40*t))*3*canvas.width/1016
+    +2*cos(28*t)-cos(29*t)+cos(30*t)-cos(31*t)+cos(32*t)-cos(33*t)+cos(34*t)-cos(35*t)+cos(40*t))*scale
 }
 
 function Y(t){
     return  canvas.height/2+(11*cos(1*t)+2*cos(2*t)-8*cos(3*t)+7*cos(4*t)-5*cos(5*t)-6*cos(6*t)+6*cos(7*t)-7*cos(8*t)-cos(9*t)+2*cos(10*t)-9*cos(11*t)+cos(12*t)-4*cos(13*t)- 
 9*cos(14*t)+5*cos(15*t)-2*cos(17*t)+11*cos(18*t)-2*cos(19*t)-cos(20*t)-2*cos(21*t)+3*cos(22*t)+cos(23*t)+cos(24*t)-cos(25*t)-2*cos(26*t)+3*cos(27*t)-cos(28*t)+ 
-cos(29*t)-cos(30*t)+cos(32*t)-cos(34*t)-cos(38*t)+cos(39*t)+cos(41*t)+cos(51*t))*3*canvas.height/980
+cos(29*t)-cos(30*t)+cos(32*t)-cos(34*t)-cos(38*t)+cos(39*t)+cos(41*t)+cos(51*t))*scale
 }
 let lastTime = 0;
 let dt = 0;
